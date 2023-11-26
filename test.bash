8e1aa10a98c5fe9a777e42f298791c6f3d23958f
Shinnosuke Nonaka <nonakashinnosuke@gmail.com>
2023-11-26 23:14:52 +0900

    READEME

--- test.bash
+++ test.bash
@@ -12,13 +12,14 @@ res=0
   [ "${out}" = 15 ]  ng ${LINENO}
     　 
   ### STRANGE INPUT ###
-  out=$(echo あ | ./plus)
+  out=$(echo あ | ./plus) 
   [ "$?" = 1 ]       ng ${LINENO}
   [ "${out}" = "" ]  ng ${LINENO}
-    　 
+   
   out=$(echo | ./plus) #空文字
   [ "$?" = 1 ]       ng ${LINENO}
   [ "${out}" = "" ] || ng ${LINENO}
+  
     　 
 [ "$res" = 0 ] && echo OK
   exit $res
